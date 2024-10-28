d86324ba512038f00f1cce661b16953b9c1f596f
Scoby77 <iangabriel_videogames@hotmail.com>
2024-10-28 10:33:30 -0600

    Archivos de la actividad 1 y 2 de Word
    Los archivos exportados de la Base de datos

---
+++ Parcial 2 China/drangonball.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `drangonball`
+ */
+
+/* `drangonball`.`characters` */
+$characters = array(
+);
+
+/* `drangonball`.`character_transformations` */
+$character_transformations = array(
+);
+
+/* `drangonball`.`tournaments` */
+$tournaments = array(
+);
+
+/* `drangonball`.`tournament_participants` */
+$tournament_participants = array(
+);
+
+/* `drangonball`.`transformations` */
+$transformations = array(
+);
+
+/* `drangonball`.`users` */
+$users = array(
+);
